0770ecb44db97c7c856a6c0e994301ffca1e2d0c
Christopher Radford <80723977+OneWhiteBird@users.noreply.github.com>
2021-04-05 23:59:44 +0100

    Fixed scripts

--- Linux.sh
+++ Linux.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "SHA256 OF CTEMPLAR.HTML ON https://ctemplar.com"
-curl https://mail.ctemplar.com | sha256sum
+curl -s https://mail.ctemplar.com | sha256sum
 echo "SHA256 OF CTEMPLAR.HTML ON https://gh.ctemplar.com"
-curl https://gh.ctemplar.com | sha256sum
+curl -s  https://gh.ctemplar.com | sha256sum
 echo "SHA256 OF CTEMPLAR.HTML ON GITHUB"
-curl https://raw.githubusercontent.com/CTemplar/webclient/gh-pages/index.html | sha256sum
-pause
+curl -s https://raw.githubusercontent.com/CTemplar/webclient/gh-pages/index.html | sha256sum
--- macOS.sh
+++ macOS.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 echo "SHA256 OF CTEMPLAR.HTML ON https://ctemplar.com"
-curl https://mail.ctemplar.com | openssl sha256
+curl -s https://mail.ctemplar.com | openssl sha256
 echo "SHA256 OF CTEMPLAR.HTML ON https://gh.ctemplar.com"
-curl https://gh.ctemplar.com | openssl sha256
+curl -s https://gh.ctemplar.com | openssl sha256
 echo "SHA256 OF CTEMPLAR.HTML ON GITHUB"
-curl https://raw.githubusercontent.com/CTemplar/webclient/gh-pages/index.html | openssl sha512
-pause
+curl -s https://raw.githubusercontent.com/CTemplar/webclient/gh-pages/index.html | openssl sha256
